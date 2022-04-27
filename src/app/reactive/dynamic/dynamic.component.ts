722172b2315d4c9e71a4333363d90051fd8fc9e9
Juan Lavallen <juanlavallen02@gmail.com>
2022-04-27 15:37:15 -0300

    dynamic forms terminados

--- src/app/reactive/dynamic/dynamic.component.ts
+++ src/app/reactive/dynamic/dynamic.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic',
@@ -8,9 +15,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.fb.array([
+      ['GTA V', Validators.required],
+      ['Counter Strike 1.6', Validators.required]
+    ], Validators.required)
+  });
+
+  newGameFavorite: FormControl = this.fb.control('', Validators.required)
+
+  get favoritesArr() {
+    return this.myForm.get('favorites') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  invalidField(field: string) {
+    return this.myForm.controls[field].invalid &&
+      this.myForm.controls[field].touched;
+  }
+
+  addFavorite() {
+    if (this.newGameFavorite.invalid) { return };
+
+    // this.favoritesArr.push(new FormControl(
+    //   this.newGameFavorite.value, Validators.required
+    // ));
+
+    this.favoritesArr.push(this.fb.control(
+      this.newGameFavorite.value, Validators.required
+    ));
+
+    this.newGameFavorite.reset();
+  }
+
+  deleteFormControl(i: number) {
+    this.favoritesArr.removeAt(i);
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset();
+    console.log(this.myForm.value);
+  }
 }
