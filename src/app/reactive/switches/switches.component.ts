a84b6b92e503d81430a42cf89a6c68fa5f07a396
Juan Lavallen <juanlavallen02@gmail.com>
2022-04-28 12:22:47 -0300

    creando inputs necesarios y conectando con el html

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    notifications: [true, Validators.required],
+    terms: [false, Validators.requiredTrue]
+  });
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
