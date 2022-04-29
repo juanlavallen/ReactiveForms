324f7fbe69144c7daf82cd697cfcf04b86ad95c7
Juan Lavallen <juanlavallen02@gmail.com>
2022-04-28 21:08:50 -0300

    LazyLoad Auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
     path: 'reactive',
     loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule)
   },
-  { path: '**', redirectTo: 'template' }
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  { path: '**', redirectTo: 'reactive' }
 ];
 
 @NgModule({
