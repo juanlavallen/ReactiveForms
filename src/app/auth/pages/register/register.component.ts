42b4eba26b11e3fda4d343192a448206edcd560c
Juan Lavallen <juanlavallen02@gmail.com>
2022-05-01 00:26:54 -0300

    validaciones en campos del formulario

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ValidatorsService } from 'src/app/shared/validators/validators.service';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.validatorService.fullnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorService.canNotStrider]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', [Validators.required]]
+  }, {
+    validators: [this.validatorService.verifyFields('password', 'confirmPassword')]
+  });
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorsService
+  ) { }
 
+  ngOnInit(): void {}
 }
