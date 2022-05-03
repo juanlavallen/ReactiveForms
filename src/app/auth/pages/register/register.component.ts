6d800da1a08d1e09efd43423f42a8d6b7360e453
Juan Lavallen <juanlavallen02@gmail.com>
2022-05-03 15:03:15 -0300

    validaciones en el campo email

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 
 import { ValidatorsService } from 'src/app/shared/validators/validators.service';
 
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.validatorService.fullnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailService]],
     username: ['', [Validators.required, this.validatorService.canNotStrider]],
     password: ['', [Validators.required, Validators.minLength(8)]],
     confirmPassword: ['', [Validators.required]]
@@ -21,10 +22,23 @@ export class RegisterComponent implements OnInit {
     validators: [this.validatorService.verifyFields('password', 'confirmPassword')]
   });
 
+  get messageErrorEmail(): string {
+    const errors = this.registerForm.get('email')?.errors;
+    if (errors?.['required']) {
+      return 'El email es obligatorio'
+    } else if (errors?.['pattern']) {
+      return 'El email no es valido'
+    } else if (errors?.['existsEmail']) {
+      return 'El email ya existe'
+    }
+    return '';
+  }
+
   constructor(
     private fb: FormBuilder,
-    private validatorService: ValidatorsService
-  ) { }
+    private validatorService: ValidatorsService,
+    private emailService: EmailValidatorService
+  ) {}
 
   ngOnInit(): void {
     this.registerForm.reset({
