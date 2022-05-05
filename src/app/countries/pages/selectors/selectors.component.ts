e3e61c7ec1ff5e313329c36af1f2c2d17b07737a
Juan Lavallen <juanlavallen02@gmail.com>
2022-05-05 18:10:36 -0300

    formGroup creado

--- src/app/countries/pages/selectors/selectors.component.ts
+++ src/app/countries/pages/selectors/selectors.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountrySmall } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-selectors',
@@ -9,7 +10,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class SelectorsComponent implements OnInit {
 
-  selectForm: FormGroup = this.fb.group({});
+  regions: string[] = [];
+  countries: CountrySmall[] = [];
+  borders: string[] = [];
+  loading: boolean = false;
+
+  selectForm: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    border: ['', [Validators.required]]
+  });
 
   constructor(private fb: FormBuilder) { }
 
