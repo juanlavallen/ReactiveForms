a2d3ca70e3ce7e1f24e68be7b767f7d07477f769
Juan Lavallen <juanlavallen02@gmail.com>
2022-05-05 18:23:30 -0300

    metodo para obtener la region

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
+import { CountrySmall } from '../interfaces/country.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +17,10 @@ export class CountriesService {
     return [...this._regions];
   }
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  
+  getCountryByRegion(region: string): Observable<CountrySmall[]> {
+    const url = `${this._baseUrl}/region/${region}?fields=name,alpha3Code`;
+    return this.http.get<CountrySmall[]>(url);
+  }
 }
