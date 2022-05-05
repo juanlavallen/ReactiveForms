001f8bfb953bae2521ce1676350a10dbad0600fe
Juan Lavallen <juanlavallen02@gmail.com>
2022-05-05 18:22:16 -0300

    propiedades _regions & _baseUrl

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private _regions: string[] = ['Africa', 'Americas', 'Europe', 'Asia', 'Oceania'];
+  private _baseUrl: string = environment.baseUrl;
+
+  get regions(): string[] {
+    return [...this._regions];
+  }
+
   constructor() { }
+
 }
